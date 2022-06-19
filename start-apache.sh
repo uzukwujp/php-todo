8b22aebef0970daafe7ac135d7dcbb6ea66a6a87
unknown <jchuks69@gmail.com>
2022-06-19 17:26:53 +0100

    updated

--- start-apache.sh
+++ start-apache.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-composer install --no-progress --no-interaction
+composer install  --no-interaction
+composer update --no-interaction
 php artisan migrate 
 php artisan key:generate 
 php artisan serve  --host=0.0.0.0  
