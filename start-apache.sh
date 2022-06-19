a8dd3273e9b4a4402b96c6fb7fe33c38f19337de
unknown <jchuks69@gmail.com>
2022-06-19 15:03:33 +0100

    updated

--- start-apache.sh
+++ start-apache.sh
@@ -3,7 +3,7 @@
 composer install --no-progress --no-interaction
 php artisan migrate 
 php artisan key:generate 
-php artisan serve  --port=$PORT --host=0.0.0.0 --env=.env 
-exec docker.php.entrypoint  "$@"
+php artisan serve  --host=0.0.0.0  
+
 
 
